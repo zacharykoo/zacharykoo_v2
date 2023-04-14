c6b03ebc7405663552ab413ae64ab693f9c437be
Zachary <mhkoo@ucalgary.ca>
2023-04-14 06:42:26 -0600

    Changes on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -52,18 +52,25 @@ const about = () => {
                             Biography
                         </h2>
                         <p className='font-medium'>
-                            Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
-                            and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
-                            new and innovative ways to bring my clients' visions to life.
+                        As a firm believer in personal growth, I am constantly seeking out new ways to 
+                        expand my skill set and make a positive impact on the world. Whether it's learning 
+                        a new programming language, mastering a new tool or technology, or taking on a 
+                        challenging project, I am always looking for ways to push myself to new heights. 
+                        With a passion for growth and development, I am excited to see where my journey 
+                        in the world of software development will take me next.
                         </p>
                         <p className='font-medium my-4'>
-                            I believe that design is about more than just making things look pretty – it's about solving problems and 
-                            creating intuitive, enjoyable experiences for users. 
+                        Design is my passion! I believe that it's not just about making things 
+                        look pretty, it's about creating problem-solving masterpieces that 
+                        provide intuitive and enjoyable experiences for users.
                         </p>
                         <p className='font-medium'>
-                            Whether I'm working on a website, mobile app, or 
-                            other digital product, I bring my commitment to design excellence and user-centered thinking to 
-                            every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
+                        Whether I'm working on a website, mobile app, or any other digital product, 
+                        I always bring my commitment to design excellence and user-centered thinking to the table. 
+                        I love nothing more than crafting designs that are both functional and visually stunning, 
+                        helping businesses and individuals stand out from the crowd. 
+                        So let's get creative and make something amazing together! 
+                        Give me a shout and let's bring your next project to life!
                         </p>
                     </div>
                     <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark
@@ -75,7 +82,7 @@ const about = () => {
                     <div className='cql-span-2 flex flex-col items-end justify-between'>
                         <div className='flex flex-col items-end justify-center'>
                             <span className='inline-block text-7xl font-bold'>
-                                <AnimaatedNumbers value={50} />+
+                                <AnimaatedNumbers value={40} />+
                             </span>
                             <h2 className='text-xl font-medium capitalize text-dark/75'>
                                 satisfied clients
@@ -83,7 +90,7 @@ const about = () => {
                         </div>
                         <div className='flex flex-col items-end justify-center'>
                             <span className='inline-block text-7xl font-bold'>
-                                <AnimaatedNumbers value={40} />+
+                                <AnimaatedNumbers value={15} />+
                             </span>
                             <h2 className='text-xl font-medium capitalize text-dark/75'>
                                 projects completed
