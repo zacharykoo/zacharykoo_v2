7c9b106c3868063b2ad2e3357509adc89299a22a
Zachary <mhkoo@ucalgary.ca>
2023-04-25 12:08:37 -0600

    Change primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,8 @@ module.exports = {
       colors: {
         dark: "#1b1b1b",
         light: "#f5f5f5",
-        primary: "#B63E96", // 240,86,199
-        primaryDark: "#58E6D9", // 80,230,217
+        primary: "#917cb5",
+        primaryDark: "#58E6D9",
       },
       animation: {
         'spin-slow': 'spin 8s linear infinite',
