19d8bee793bc4cede6d0bc3e1fb3007f7caa589d
Zachary Koo <mhkoo@ucalgary.ca>
2025-11-26 01:13:25 -0700

    added next export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export', // tells Next.js to generate static HTML
+  basePath: isProd ? '/zacharykoo_v2' : '', // project repo name
+  assetPrefix: isProd ? '/zacharykoo_v2/' : '', // ensures assets load correctly
+  images: {
+    unoptimized: true, // disable server-side image optimization
+  },
 }
 
 module.exports = nextConfig
