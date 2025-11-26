5e53fa336ee00ec37b003dd83c3f2e0206207d8a
Zachary Koo <mhkoo@ucalgary.ca>
2025-11-26 01:18:36 -0700

    Set basePath, assetPrefix, images.unoptimized for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
-  reactStrictMode: true,
-}
+  output: 'export',
+  basePath: '/zacharykoo_v2',
+  assetPrefix: '/zacharykoo_v2/',
+  images: { unoptimized: true },
+};
 
 module.exports = nextConfig
